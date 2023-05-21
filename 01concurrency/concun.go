c618593ab7e6a49bd6640d9b4dae64527843a923
Jitendra wadle <jitendrawadle@gmail.com>
2023-05-21 19:07:59 +0530

    compare func
    Compare normal fun and Concurrent fun.

--- 01concurrency/concun.go
+++ 01concurrency/concun.go
@@ -3,20 +3,39 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	fmt.Println("Learn concurrency")
 	var wg sync.WaitGroup
-	wg.Add(10)
-	for i := 0; i < 10; i++ {
+	timeCurrent := time.Now()
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
 		go conCurrentfun(&wg)
 	}
-	wg.Wait()
 
+	totalTimeForConCurrent := time.Since(timeCurrent)
+	fmt.Printf("Total time taken %v\n", totalTimeForConCurrent)
+
+	normalCurrent := time.Now()
+	for i := 0; i < 100; i++ {
+		myFunc()
+	}
+	fmt.Printf("Normal Fun Total time taken %v\n", time.Since(normalCurrent))
+	fmt.Printf("Concurrent Fun Total time taken %v\n", totalTimeForConCurrent)
+
+	wg.Wait()
+	// OutPut:
+	//Normal Fun Total time taken 15.0317ms
+	//Concurrent Fun Total time taken 505.8µs
 }
 
 func conCurrentfun(wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("I am running for conCurrency")
+}
+
+func myFunc() {
+	fmt.Println("Normal func running")
 }
